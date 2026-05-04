e92743bb4afd966fdba4accc0708ace5a6cc1b08
Akash Ruppa <akashruppa42a2@gmail.com>
2026-05-04 07:51:47 +0000

    DAY-13:Upload

---
+++ DAY-13/README.md
@@ -0,0 +1,7 @@
+# Day 13 - Linux + Python
+
+## Python
+- functions (def,return)
+
+## Linux
+   --------
---
+++ DAY-13/fun.py
@@ -0,0 +1,52 @@
+#Functions
+"""
+def my_function():
+    print("HII..")
+
+def name(first,last):
+    first = first.capitalize()
+    last  = last.capitalize()
+    print(f"{first}  {last}")
+name("akash","ruppa")
+def new_function(name,age):
+    print(f"hii {name}")
+    print(f"you are {age} old")
+new_function("akash",20)
+new_function("bose",20)
+new_function("sharma",20)
+"""
+
+#TASKS
+#1--
+"""
+def sum():
+    num1 = int(input("enter num1:"))
+    num2 = int(input("enter num2:"))
+    print(f"{num1+num2}")
+sum()
+
+#2--
+def length():
+    name = input("enter your name:")
+    print(f"you'r name has {len(name)}  characters")
+length()
+#3--
+def ver():
+    num = int(input("Enter some random stuff(numbers):"))
+    if num % 2 == 0:
+        print("EVEN")
+    elif(num%2!=0):
+        print("ODD")
+ver()
+#4--
+def car():
+    name = input("enter your name:")
+    print(f"{name[0]}")
+car()
+"""
+def list():
+    numlist = ()
+    items = int(input("\n1:\n2:\n3:"))
+    return sum(numlist)
+list()
+    
