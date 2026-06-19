7b5fe89c402429c0d95c867578bd2b7ce25a307b
Akash Ruppa <akashruppa42a2@gmail.com>
2026-06-19 11:50:00 +0000

    DAY-36:UPLOAD

---
+++ DAY-36/README.md
@@ -0,0 +1,12 @@
+# Day 36 - Linux + Python
+
+## Python
+- revised nested loops
+
+
+## Linux Commands Practiced
+- $PATH
+
+## Linux file
+
+- [text](main.bash)
---
+++ DAY-36/code.py
@@ -0,0 +1,64 @@
+#-- Revise--
+"""print("HELLO")
+print("HELLO")
+print("HELLO")
+print("HELLO")
+print("HELLO")
+for i in range(5):
+   print(f"{i} HELLO")
+count = 1
+while count <= 10:
+    print(count)
+    count += 2
+
+for i in range(2,10,2):
+    print(i)
+name = "AKASH"
+for letter in name:
+    print(letter)
+
+for i in range(5):
+    for j in range(4):
+        print(i,j)
+
+#TASKS
+#1---
+for i in range(1,21):
+    print(i)
+    
+for num in range(21,1,1):
+    print(num)
+    
+for num in range(0,100,2):
+    print(num)
+
+for num in range(1,100,2):
+    print(num)
+    
+for num in range(7,71,7):
+    print(num)
+    
+#2--
+for i in range(5):
+    for j in range(3):
+       # print(i)
+        print(j)
+
+for i in range(1,6):
+    for j in range(i):
+        print("*",end="")
+    print()
+for i in range(5):
+    for j in range(i):
+        print(i)
+        
+for i in range(5):
+    for j in range(5-i):
+        print("*",end="")
+    print()
+        """
+for i in range(6):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+
