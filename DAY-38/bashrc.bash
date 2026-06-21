eef109902d6d5ab1196abb099072c60dad9b7248
Akash Ruppa <akashruppa42a2@gmail.com>
2026-06-21 17:38:29 +0000

    DAY-38:UPLOAD

---
+++ DAY-38/bashrc.bash
@@ -0,0 +1,72 @@
+akash@akash:~$ nano ~/ .bashrc
+akash@akash:~$ source ~/ .bashrc
+-bash: source: /home/akash/: is a directory
+akash@akash:~$ source ~/.bashrc
+-bash: alias: py: not found
+-bash: alias: =: not found
+-bash: alias: python3: not found
+akash@akash:~$ py
+Command 'py' not found, but can be installed with:
+sudo apt install pythonpy
+akash@akash:~$ nano ~/ .bashrc
+akash@akash:~$ py
+Command 'py' not found, but can be installed with:
+sudo apt install pythonpy
+akash@akash:~$ source ~/ .bashrc
+-bash: source: /home/akash/: is a directory
+akash@akash:~$ soruce ~/.bashrc
+soruce: command not found
+akash@akash:~$ source ~/.bashrc
+akash@akash:~$ py
+Python 3.12.3 (main, Mar 23 2026, 19:04:32) [GCC 13.3.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("AKASH")
+AKASH
+>>> x = 10
+>>> y = 10
+>>> print(x+y)
+20
+>>>
+KeyboardInterrupt
+>>>
+KeyboardInterrupt
+>>> exit
+Use exit() or Ctrl-D (i.e. EOF) to exit
+>>> exit()
+akash@akash:~$ nano ~/ .bashrc
+akash@akash:~$ source ~/ .bashrc
+-bash: source: /home/akash/: is a directory
+akash@akash:~$ source ~/.bashrc
+Hii Akash
+akash@akash:~$
+akash@akash:~$ nano ~/ .bashrc
+akash@akash:~$ source ~/.bashrc
+Hii Akash
+akash@akash:~$ ll
+total 88
+drwxr-xr-x  2 akash akash 4096 Jun 11 11:41 demozips
+drwxr-xr-x  6 akash akash 4096 Jun 15 11:42 developer-lab
+drwxr-xr-x  5 akash akash 4096 Jun  4 11:48 env
+-rw-r--r--  1 akash akash   88 Jun  4 17:17 env.py
+drwxr-xr-x  2 akash akash 4096 May 22 17:08 files
+drwxr-xr-x  4 akash akash 4096 Jun  9 19:07 find-practice
+drwxr-xr-x  2 akash akash 4096 Jun 15 11:47 hi
+drwxr-xr-x  5 akash akash 4096 Jun 15 11:34 linux-fs-practice
+-rw-r--r--  1 akash akash  148 Jun  2 17:26 morestudents.json
+-rw-r--r--  1 akash akash   18 Jun  4 17:11 name.txt
+drwxr-xr-x  2 akash akash 4096 May 22 17:10 nan-vi
+-rwxr-xr-x  1 akash akash    0 May  4 07:47 newfile.txt
+-rw-r--r--  1 akash akash   22 Jun  1 17:19 notes.txt
+-rw-r--r--  1 akash akash   51 Jun  1 17:20 numbers.txt
+drwxr-xr-x  2 akash akash 4096 May 19 17:48 path
+drwxr-xr-x  3 akash akash 4096 Apr 14 15:38 practice
+drwxr-xr-x 42 akash akash 4096 Jun 20 04:48 python-linux-journey
+drwxr-xr-x  2 akash akash 4096 Jun 11 17:33 pythonfolders
+drwxr-xr-x  2 akash akash 4096 May 17 17:00 random
+drwxr-xr-x  2 akash akash 4096 Jun 20 04:45 scripts
+-rwxr-xr-x  1 akash akash   14 May  5 06:50 simple.sh
+-rwxr-xr-x  1 akash akash   43 May  5 06:52 simple2.sh
+-rwxr-xr-x  1 akash akash   32 Jun 19 03:38 text.sh
+akash@akash:~$ cd ~/python-linux-journey
+akash@akash:~/python-linux-journey$ code .
+akash@akash:~/python-linux-journey$
