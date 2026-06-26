a1da403c941db10ca82d4b7a9280a2f618b793fa
Akash Ruppa <akashruppa42a2@gmail.com>
2026-06-26 18:19:22 +0000

    DAY-43:UPLOAD

---
+++ DAY-43/CRON.sh
@@ -0,0 +1,107 @@
+Welcome! Akash bhAAi....
+akash@akash:~$ cd ~/python-linux-journey
+akash@akash:~/python-linux-journey$ code .
+akash@akash:~/python-linux-journey$ cd ~
+akash@akash:~$ mkdir cron_lab
+akash@akash:~$ cd cron_lab
+akash@akash:~/cron_lab$ nano hello.sh
+akash@akash:~/cron_lab$ chmod +x hello.sh
+akash@akash:~/cron_lab$ ./hello.sh
+Cron-practice
+Fri Jun 26 17:51:09 UTC 2026
+akash@akash:~/cron_lab$ crontab -e
+no crontab for akash - using an empty one
+
+Select an editor.  To change later, run 'select-editor'.
+  1. /bin/nano        <---- easiest
+  2. /usr/bin/vim.basic
+  3. /usr/bin/vim.tiny
+  4. /bin/ed
+
+Choose 1-4 [1]: 1
+crontab: installing new crontab
+akash@akash:~/cron_lab$ cat cron.log
+cat: cron.log: No such file or directory
+akash@akash:~/cron_lab$ cat cron.log
+Cron is working
+akash@akash:~/cron_lab$ cat cron.log
+Cron is working
+Cron is working
+Cron is working
+akash@akash:~/cron_lab$ cat cron.log
+Cron is working
+Cron is working
+Cron is working
+akash@akash:~/cron_lab$ nano cron.lo
+akash@akash:~/cron_lab$ nano cron.log
+akash@akash:~/cron_lab$ cat cron.log
+Cron is working
+Cron is working
+Cron is working
+Cron is working
+akash@akash:~/cron_lab$ nano cron.log
+akash@akash:~/cron_lab$ cat cron.log
+Cron is working
+Cron is working
+Cron is working
+Cron is working
+echo date time
+akash@akash:~/cron_lab$ sudo service cron satus
+[sudo] password for akash:
+ * Usage: /etc/init.d/cron {start|stop|status|restart|reload|force-reload}
+akash@akash:~/cron_lab$ nano cron.log
+akash@akash:~/cron_lab$ cat cron.log
+Cron is working
+Cron is working
+Cron is working
+Cron is working
+Cron is working
+Cron is working
+date
+Cron is working
+akash@akash:~/cron_lab$ cat cron.log
+Cron is working
+Cron is working
+Cron is working
+Cron is working
+Cron is working
+Cron is working
+date
+Cron is working
+akash@akash:~/cron_lab$ nano cron.log
+akash@akash:~/cron_lab$ crontab -e
+crontab: installing new crontab
+akash@akash:~/cron_lab$ cat cron.sh
+cat: cron.sh: No such file or directory
+akash@akash:~/cron_lab$ crontab -e
+crontab: installing new crontab
+akash@akash:~/cron_lab$ cat cron.sh
+cat: cron.sh: No such file or directory
+akash@akash:~/cron_lab$ cat cron.sh
+cat: cron.sh: No such file or directory
+akash@akash:~/cron_lab$ cat cron.sh
+Cron is working
+akash@akash:~/cron_lab$ nano cron.sh
+akash@akash:~/cron_lab$ cat cron.sh
+#/bin/bash
+Cron is working
+Cron is working
+date
+akash@akash:~/cron_lab$ cat cron.sh
+#/bin/bash
+Cron is working
+Cron is working
+date
+Cron is working
+akash@akash:~/cron_lab$ nano cron.sh
+akash@akash:~/cron_lab$ crontab -e
+crontab: installing new crontab
+akash@akash:~/cron_lab$ cat cron.sh
+#/bin/bash
+Cron is working
+Cron is working
+Cron is working
+Cron is working
+Cron is working
+Cron is working
+Fri Jun 26 18:10:01 UTC 2026s
